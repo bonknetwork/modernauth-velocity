f8d76eb10aa767c2c311783a6fad67afcfff7b48
shloakvatsyayan <shloak.vatsyayan@gmail.com>
2025-03-15 13:22:45 -0400

    Initial commit

---
+++ build/generated/sources/templates/org/bonkmc/modernAuthenticationVelocity/BuildConstants.java
@@ -0,0 +1,7 @@
+package org.bonkmc.modernAuthenticationVelocity;
+
+// The constants are replaced before compilation
+public class BuildConstants {
+
+    public static final String VERSION = "1.0";
+}
